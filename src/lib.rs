886dd1cd12c0fa6bf3c855e821e1c84cac7b3196
motii <108280115+motii8128@users.noreply.github.com>
2023-12-22 09:45:43 +0900

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,13 @@ pub mod error;
 pub mod launch;
 pub mod param;
 
-pub fn g_file()
-{
+use async_std::{io::BufReader, fs::File};
+use quick_xml::reader::Reader;
 
+pub fn get_xml(path:String)->Reader<B>
+{
+    let mut reader = Reader::from_file(path).unwrap();
+    reader.trim_text(true);
+
+    reader
 }
