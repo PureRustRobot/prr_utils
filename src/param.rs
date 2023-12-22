6d4a621885c10e2f9a7cbcde7ba3e2912e3b56f8
motii <108280115+motii8128@users.noreply.github.com>
2023-12-22 17:52:15 +0900

    Add files via upload

--- src/param.rs
+++ src/param.rs
@@ -9,5 +9,26 @@ pub fn get_param(mut reader:Reader<BufReader<File>>, name:&str)->String
 
     let mut buf = Vec::new();
 
-    
+    loop {
+        match reader.read_event_into(&mut buf) {
+
+            Ok(Event::Start(e)) => {
+                let elm_name = String::from_utf8(e.name().as_ref().to_vec()).unwrap();
+                if elm_name == name {
+                    flg = true
+                }
+            }
+
+            // テキストイベント
+            Ok(Event::Text(e)) => {
+                if flg {
+                    return e.unescape().unwrap().to_string();
+                }
+            }
+
+            // その他のイベントは何もしない
+            _ => (),
+        }
+        buf.clear();  // メモリ節約のためbufをクリアする
+    }
 }
