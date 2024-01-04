360f4f4a49d236a96e1c9b64310dd071e06567c6
motii <108280115+motii8128@users.noreply.github.com>
2024-01-04 16:12:48 +0900

    Add files via upload

--- src/logger.rs
+++ src/logger.rs
@@ -2,9 +2,9 @@ use colored::{self, Colorize};
 
 pub fn log_info(bin_name:&str, msg:String)
 {
-    println!("{}:{}", bin_name.bright_blue(), msg.bright_blue());
+    println!("[{}]:{}", bin_name.bright_blue(), msg.bright_blue());
 }
 pub fn log_error(bin_name:&str, msg:String)
 {
-    println!("{}:{}", bin_name.bright_red(), msg.bright_red());
+    println!("[{}]:{}", bin_name.bright_red(), msg.bright_red());
 }
